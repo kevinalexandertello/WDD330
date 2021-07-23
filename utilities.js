2817541e868eabd9fb1ea2db96b1c33de35f773e
kevinalexandertello <78240602+kevinalexandertello@users.noreply.github.com>
2021-07-23 16:35:51 -0300

    Add files via upload

--- utilities.js
+++ utilities.js
@@ -1,19 +1,15 @@
-export function getJSON(url) {
-    return fetch(url)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        } else {
-          //console.log(response.json());
-          return response.json();
-        }
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
-  export const getLocation = function(options) {
-    return new Promise(function(resolve, reject) {
-      navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
-  };
+
+
+function querySelector(selector){
+    const element = document.querySelector(selector)
+    if (element) return element;
+    else return null;
+
+}
+// function onClick (elementSelector ,cb){
+//     elementSelector.addEventListener('click',cb)
+
+// }
+
+
+export { querySelector}
